1d3810c9bcf45f631b135b7505930438a261f902
Harshal Shirote <harshalshirote2002@gmail.com>
2024-09-29 21:12:15 +0530

    Completion: Shop Page General

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,5 +1,6 @@
 import "./css/shop.css";
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export function Shop() {
   const [allProducts, setAllProducts] = useState(null);
@@ -7,17 +8,28 @@ export function Shop() {
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((json) => setAllProducts(json))
+      .then((json) => {
+        setAllProducts(json);
+        console.log(json);
+      });
   }, []);
 
   return allProducts == null ? (
     <div className="loading-shop">Loading...</div>
   ) : (
     <div className="loaded-shop">
-      {allProducts.map((obj, id) => (
-        <div className= "card" key={id}>
-            <img key={id} src={obj.image}></img>
-            <p>{obj.title}</p>
+      {allProducts.map((obj) => (
+        <div className="card" key={uuidv4()}>
+          <div className="image-holder">
+            <img key={uuidv4()} src={obj.image}></img>
+          </div>
+          <div className="general-holder">
+            <p className="loaded-shop-title">{obj.title.slice(0, 45)}...</p>
+            <div className="item-details">
+              <p>Add to Cart</p>
+              <p className="loaded-shop-price">${obj.price}</p>
+            </div>
+          </div>
         </div>
       ))}
     </div>
