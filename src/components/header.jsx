88036cd1eed4d2cb0a382c323f6f929765e0eed4
Harshal Shirote <harshalshirote2002@gmail.com>
2025-01-02 11:15:36 +0530

    Addition: Cart count functionality in the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,15 @@
 import reactIcon from "../assets/react.svg"
 import { Link } from "react-router-dom";
 
-export function Header() {
+export function Header(props) {
+  const cart = props.cart;
   return <header>
     <img src={reactIcon}></img>
     <div className="nav-elements">
       <Link to = "/">Home</Link>
       <Link to = "shop">Shop</Link>
-      <Link to = "cart">Cart</Link>
+      <Link to = "cart">Cart {cart? cart.reduce((total, current)=>total+current.count, 0): 0}
+      </Link>
     </div>
   </header>;
 }
